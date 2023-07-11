cc2830db22fea8085803f00b594da6a2b1c6381c
Andrew Williams <andrew.k.w@outlook.com>
2023-07-11 10:46:38 +0100

    set up basic react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import "./App.css";
+// react-router
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+// pages
+import HomePage from "./pages/HomePage";
+// components
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <h1>APP start</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
