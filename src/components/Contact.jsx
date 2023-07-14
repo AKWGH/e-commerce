e383e8d51a990a97a08ead0c31b58a9a13e4942a
Andrew Williams <andrew.k.w@outlook.com>
2023-07-14 17:19:20 +0100

    newsletter styling complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="contact-section">
+      <h3>Join our newsletter and get 20% off</h3>
+      <div className="contact-content">
+        <p>
+          Unlock a world of exclusive offers, captivating content, and
+          delightful surprises by joining our newsletter today. Stay connected
+          and be inspired!
+        </p>
+        <form action="" className="contact-form">
+          <input
+            type="email"
+            className="form-input"
+            placeholder="enter email"
+          />
+          <button type="submit" className="submit-btn">
+            subscribe
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
